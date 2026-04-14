f4fc10f6bd0ba3d27b53805699476f5e0991edb5
baileykaat <baileykaat@gmail.com>
2026-04-14 17:39:20 -0400

    chore: initial project setup
    primary packages creation

---
+++ src/main/java/com/docx/docxrenderer/DocxRendererApplication.java
@@ -0,0 +1,13 @@
+package com.docx.docxrenderer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocxRendererApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocxRendererApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/docx/docxrenderer/DocxRendererApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docx.docxrenderer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocxRendererApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
